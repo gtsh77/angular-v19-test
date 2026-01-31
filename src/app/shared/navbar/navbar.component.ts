de8a40f5ab50060427cd0b85d9976e6fedb57837
Anton Makridin <a.i.makridin@yandex.ru>
2026-01-31 12:58:55 +0100

    footer, compass mixins added, navbar upd

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   public url = ""
+  public isCollapsed: boolean = true;
+
+  public checkUrl(string: string): boolean {
+    if(this.url.indexOf(string) > - 1) return true;
+    else return false;
+  }
+
   constructor(private router:Router) {
     //watch for route url change
     router.events.subscribe((route) => {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent }  from './navbar/navbar.component';
+import { FooterComponent }  from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
@@ -8,10 +9,12 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     NavbarComponent,
+    FooterComponent,
     RouterModule
   ],
   exports: [
     NavbarComponent,
+    FooterComponent,
     RouterModule
   ]
 })
